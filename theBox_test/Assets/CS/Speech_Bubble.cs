d72effd4fb1cda55297f11d487a94f9608196a19
SironX <kagetouki@gmail.com>
2021-09-13 21:41:48 +0800

    新增可愛的小東西，以及秘密.txt

---
+++ theBox_test/Assets/CS/Speech_Bubble.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+public class Speech_Bubble : MonoBehaviour
+{
+    public Image Bubble;
+    public Text txt;
+    public string[] Strings;
+    public float popupTime;
+    public float AwaitTime;
+    public bool floating = false;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            StartCoroutine(ShowCode());
+            floating = true;
+        }
+
+        if (floating)
+        {
+
+        }
+    }
+
+    IEnumerator ShowCode()
+    {
+        while (Bubble.color.a < 1)
+        {
+            Bubble.color = new Color(Bubble.color.r, Bubble.color.g, Bubble.color.b, Bubble.color.a + Time.deltaTime);
+            yield return new WaitForFixedUpdate();
+        }
+
+        foreach (char letter in Strings[0].ToCharArray())
+        {
+            txt.text += letter;
+
+            yield return new WaitForSeconds(AwaitTime);
+        }
+    }
+}
---
+++ theBox_test/Assets/CS/Speech_Bubble_Shake.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Speech_Bubble_Shake : MonoBehaviour
+{
+    public float speed;
+    public Vector3 direction;
+
+    public bool useDirection2 = false;
+    public Vector3 direction2;
+    public float halfCycleTime;
+    float timer;
+    bool gotoDirection2 = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (useDirection2)
+        {
+            timer += Time.deltaTime;
+
+            if (gotoDirection2)
+            {
+                transform.localPosition = Vector3.Lerp(direction, direction2, timer / halfCycleTime);
+            }
+            else
+            {
+                transform.localPosition = Vector3.Lerp(direction2, direction, timer / halfCycleTime);
+            }
+
+            if (timer >= halfCycleTime)
+            {
+                timer = 0;
+                gotoDirection2 = !gotoDirection2;
+            }
+        }
+        else
+        {
+            GetComponent<RectTransform>().localPosition += direction * speed * Time.deltaTime;
+        }
+
+    }
+}
